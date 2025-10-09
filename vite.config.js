027c9c1c97e447851be2b3df3720d9f0c482ee71
JoannMr <145122335+JoannMr@users.noreply.github.com>
2025-10-09 18:54:32 +0200

    Nuevo favicon

--- vite.config.js
+++ vite.config.js
@@ -2,39 +2,32 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
-    minify: 'terser',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        '404': resolve(__dirname, '404.html'),
         'avis-legal': resolve(__dirname, 'avis-legal.html'),
         'privacitat': resolve(__dirname, 'privacitat.html'),
-        'cookies': resolve(__dirname, 'cookies.html'),
-        '404': resolve(__dirname, '404.html')
-      },
-      output: {
-        manualChunks: {
-          vendor: ['gsap']
-        }
+        'cookies': resolve(__dirname, 'cookies.html')
       }
-    }
+    },
+    copyPublicDir: true
   },
+  publicDir: 'src/img/favicon',
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "./src/styles/variables.scss";`
       }
-    },
-    devSourcemap: true
+    }
   },
   server: {
     port: 3000,
-    open: true,
-    host: true
+    open: true
   },
   preview: {
     port: 4173,
